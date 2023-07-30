c90465a07f6539dae554bab95d2c5eaf118a03ea
phoeinx <hradilak@uni-potsdam.de>
2023-07-30 12:34:27 +0200

    Initialize repository.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # wg-notifications
-Receive notifications if a new flat share offer is published on flat share websites.
+
+Send push notifications via Pushover when there are new flat share (German: WG) offers on [wg-gesucht.de](https://wg-gesucht.de/).
+
+## Setup
+
+* Fork this repository.
+* Download [Pushover](https://pushover.net/), create an account, and [register an application](https://pushover.net/api).
+* Set up the following [repository secrets](https://docs.github.com/en/actions/security-guides/encrypted-secrets#creating-encrypted-secrets-for-a-repository): `PUSHOVER_API_TOKEN`, `PUSHOVER_USER_KEY`.
+* Adjust the [configuration file](config.yml) to your needs, i.e. adjust from which date or for what type of flat you are searching.
+* Rename `.github/workflows/run.yml.example` to `.github/workflows/run.yml`.
+* Done! A scheduled GitHub Action executes the script every 15 minutes on weekdays. You will receive a Pushover notification whenever new offers are found.
---
+++ index.js
@@ -0,0 +1,111 @@
+import { chromium } from 'playwright-chromium';
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+import fs from 'fs';
+import YAML from 'yaml';
+
+dotenv.config();
+
+const configFile = fs.readFileSync('./config.yml', 'utf8');
+const config = YAML.parse(configFile);
+
+if (!config.url || !config.type || !config.town) {
+  console.log('Please make sure to provide valid config file.');
+  process.exit(1);
+}
+
+const locale = 'de-DE';
+const timeZone = 'Europe/Berlin';
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto(config.url);
+
+  // print page title
+  console.log(await page.title());
+
+  // await page.waitForSelector('#id_buergerauswahldienststelle_tree-office');
+
+  // // For some municipalities, the list of offices is initially collapsed
+  // const expandButton = await page.$(
+  //   `.searchTreeItemChild:has-text('${config.office}') button[title="öffnen"]`
+  // );
+
+  // if (expandButton) {
+  //   await expandButton.click();
+  // }
+
+  // // Select office
+  // await page.click(
+  //   `.searchTreeItemChild:has-text('${config.office}') button:has-text("Termin vereinbaren")`
+  // );
+
+  // // Select services
+  // for (const [label, value] of Object.entries(config.services)) {
+  //   await page.selectOption(`text=${label}`, value.toString());
+  // }
+
+  // await page.click('button:has-text("Weiter")');
+
+  // // Confirm services
+  // await page.click('button:has-text("Weiter")');
+
+  // // Find available dates
+  // await page.waitForSelector('.ekolCalendarContainer');
+  // const available = await page.$$('.eKOLCalendarButtonDayFreeX');
+
+  // if (available.length <= 0) {
+  //   console.log('Exiting as no appointments are available.');
+  //   return await browser.close();
+  // }
+
+  // const availableSlots = Object.fromEntries(
+  //   await Promise.all(
+  //     available.map(async (element) => {
+  //       const id = await element.getAttribute('id');
+  //       const parts = id.split('||');
+  //       const timestamp = parts[parts.length - 1];
+
+  //       const slotsContainer = await element.$(
+  //         '.ekolCalendarFreeTimeContainer'
+  //       );
+  //       const slotsCount = parseInt(await slotsContainer.textContent());
+
+  //       return [timestamp, slotsCount];
+  //     })
+  //   )
+  // );
+
+  // const slotsCount = Object.values(availableSlots).reduce(
+  //   (prev, curr) => prev + curr
+  // );
+
+  // const formatDate = (timestamp) => {
+  //   const date = new Date(parseInt(timestamp));
+  //   return date.toLocaleDateString(locale, { timeZone });
+  // };
+
+  // const dates = Object.keys(availableSlots);
+  // const firstDate = dates[0];
+  // const lastDate = dates[dates.length - 1];
+
+  // const message =
+  //   `Es gibt aktuell ${slotsCount} freie Termine ` +
+  //   `zwischen ${formatDate(firstDate)} und ${formatDate(lastDate)}.`;
+
+  // console.log('Sending notification.');
+  // const res = await fetch('https://api.pushover.net/1/messages.json', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify({
+  //     token: process.env.PUSHOVER_API_TOKEN,
+  //     user: process.env.PUSHOVER_USER_KEY,
+  //     message,
+  //   }),
+  // });
+
+  await browser.close();
+})();
