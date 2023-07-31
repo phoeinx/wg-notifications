ee31d4feb1c48be4fdf9ee6f1a5d3cbdf33be1df
phoeinx <hradilak@uni-potsdam.de>
2023-07-31 18:54:25 +0200

    Add first version for flat share polling.

--- index.js
+++ index.js
@@ -9,103 +9,100 @@ dotenv.config();
 const configFile = fs.readFileSync('./config.yml', 'utf8');
 const config = YAML.parse(configFile);
 
-if (!config.url || !config.type || !config.town) {
+if (!config.url || !config.type || !config.town || !config.minutes) {
   console.log('Please make sure to provide valid config file.');
   process.exit(1);
 }
 
-const locale = 'de-DE';
-const timeZone = 'Europe/Berlin';
+if (config.minutes < 1 || config.minutes > 60) {
+  console.log('Please make sure to provide valid minutes in config file.');
+  process.exit(1);
+}
 
 (async () => {
   const browser = await chromium.launch();
   const page = await browser.newPage();
   await page.goto(config.url);
 
-  // print page title
-  console.log(await page.title());
+  // Accept cookies :/
+  const cookiesAcceptButton = await page.getByRole('button', { name: 'Accept all' })
 
-  // await page.waitForSelector('#id_buergerauswahldienststelle_tree-office');
+  if (cookiesAcceptButton) {
+    console.log("Accepting cookies :/ ");
+    await cookiesAcceptButton.click();
+  }
 
-  // // For some municipalities, the list of offices is initially collapsed
-  // const expandButton = await page.$(
-  //   `.searchTreeItemChild:has-text('${config.office}') button[title="öffnen"]`
-  // );
+  await page.locator('#autocompinp').fill('Potsdam');
+  await page.keyboard.press('Enter');
 
-  // if (expandButton) {
-  //   await expandButton.click();
-  // }
+  await page.getByRole('button', { name: '1-Zimmer-Wohnung' }).click();
+  await page.getByTitle('Rubrik').selectOption({ textContent: 'WG-Zimmer' });
+  await page.getByRole('button', { name: 'WG-Zimmer' }).click();
 
-  // // Select office
-  // await page.click(
-  //   `.searchTreeItemChild:has-text('${config.office}') button:has-text("Termin vereinbaren")`
-  // );
+  await page.getByRole('button', { name: 'Finden' }).click();
 
-  // // Select services
-  // for (const [label, value] of Object.entries(config.services)) {
-  //   await page.selectOption(`text=${label}`, value.toString());
-  // }
+  
+  //Dummy selector to wait for page to load
+  await page.waitForSelector('#partners_wrapper');
+  // TODO: check for right title
 
-  // await page.click('button:has-text("Weiter")');
+  let offerDivs = await page.locator('div[data-id]').all();
+  let offers = await Promise.all(
+    offerDivs.map(async (offer) => {
+      const online = await offer.getByText(/Online:/);
+      const cost = await offer.getByText(/€/).first(); //heuristic will be wrong sometimes
+      const title = await offer.getByRole('heading', { level: 3 });
+      const link = await offer.getByRole('link').first();
+      
+      return {
+        online: await online.textContent(),
+        cost: await cost.textContent(),
+        title: await title.textContent(),
+        link: await link.getAttribute('href')
+      }
+    }));
 
-  // // Confirm services
-  // await page.click('button:has-text("Weiter")');
+  offers = offers.filter(offer => offer.online.includes('Minuten'));
 
-  // // Find available dates
-  // await page.waitForSelector('.ekolCalendarContainer');
-  // const available = await page.$$('.eKOLCalendarButtonDayFreeX');
+  offers.forEach(offer => {
+    offer.title = offer.title.replace(/\n/g, ' ').trim();
+    offer.link = `${config.url}${offer.link}`;
+    offer.minutes = parseInt(offer.online.match(/\d+/)[0]);
+  })
 
-  // if (available.length <= 0) {
-  //   console.log('Exiting as no appointments are available.');
-  //   return await browser.close();
-  // }
+  offers = offers.filter(offer => offer.minutes <= config.minutes);
 
-  // const availableSlots = Object.fromEntries(
-  //   await Promise.all(
-  //     available.map(async (element) => {
-  //       const id = await element.getAttribute('id');
-  //       const parts = id.split('||');
-  //       const timestamp = parts[parts.length - 1];
+  let messages = offers.map(offer => {
+    return `${offer.title} - ${offer.cost} - ${offer.online} - ${offer.link}`;
+  });
 
-  //       const slotsContainer = await element.$(
-  //         '.ekolCalendarFreeTimeContainer'
-  //       );
-  //       const slotsCount = parseInt(await slotsContainer.textContent());
+  const offersCount = offers.length;
+  console.log(`Found ${offersCount} new offers.`);
 
-  //       return [timestamp, slotsCount];
-  //     })
-  //   )
-  // );
+  if (offersCount <= 0) {
+    console.log('Exiting as no new offers found.');
+    await browser.close();
+    process.exit(0);
+  }
 
-  // const slotsCount = Object.values(availableSlots).reduce(
-  //   (prev, curr) => prev + curr
-  // );
+  const message =
+    `In der letzten Stunde wurden die folgenden ${offersCount} neuen WGs ` +
+    `eingestellt: \n` +
+    messages.join('\n');
+  
+  console.log('Sending notification.');
 
-  // const formatDate = (timestamp) => {
-  //   const date = new Date(parseInt(timestamp));
-  //   return date.toLocaleDateString(locale, { timeZone });
-  // };
-
-  // const dates = Object.keys(availableSlots);
-  // const firstDate = dates[0];
-  // const lastDate = dates[dates.length - 1];
-
-  // const message =
-  //   `Es gibt aktuell ${slotsCount} freie Termine ` +
-  //   `zwischen ${formatDate(firstDate)} und ${formatDate(lastDate)}.`;
-
-  // console.log('Sending notification.');
-  // const res = await fetch('https://api.pushover.net/1/messages.json', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify({
-  //     token: process.env.PUSHOVER_API_TOKEN,
-  //     user: process.env.PUSHOVER_USER_KEY,
-  //     message,
-  //   }),
-  // });
+  const res = await fetch('https://api.pushover.net/1/messages.json', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      token: process.env.PUSHOVER_API_TOKEN,
+      user: process.env.PUSHOVER_USER_KEY,
+      message,
+    }),
+  });
 
   await browser.close();
 })();
